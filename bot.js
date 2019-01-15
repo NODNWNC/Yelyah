e83d1f68f0ca95022cd1c3f551e450063ea79e2c
NODNWNC <46719789+NODNWNC@users.noreply.github.com>
2019-01-15 16:51:58 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,15 +1,29 @@
 const Discord = require('discord.js');
+
 const client = new Discord.Client();
 
-client.on('ready',() => {
-  console.log('I\'m Online\nI\'m Online');
-})
+ 
 
-client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
-   }
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
 });
 
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+
+client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
